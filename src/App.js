028bccf7b4e5059335353ae951a724f61eca8760
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-03 14:02:48 +0800

    remove external ad, edit README.md to reflect information about project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import "./App.css";
 import NavBlock from "./components/NavBlock/NavBlock";
-import Ad from "./components/Ads/Ad";
 import StNewsTablet from "./components/Ads/StNewsTablet";
 import TopStories from "./components/TopStories/TopStories";
 
 function App() {
   return (
     <div className="bg-light">
-      {/* row for top navbar */}
-      <div>
-        <NavBlock />
-        <Ad />
-        <StNewsTablet />
-        <TopStories />
-      </div>
+      <NavBlock />
+      <StNewsTablet />
+      <TopStories />
     </div>
   );
 }
